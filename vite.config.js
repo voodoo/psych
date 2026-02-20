57f24c1f4a6dba7dc08759f78510452cba740c13
Rocky <rocky@psych.local>
2026-02-20 05:59:32 -0500

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 export default {
   server: {
-    allowedHosts: [
-      '.trycloudflare.com',
-      'localhost',
-      '127.0.0.1',
-      '172.18.0.2',
-      'pete-benefit-cabinet-profits.trycloudflare.com'
-    ]
+    allowedHosts: 'all'
   }
 }
